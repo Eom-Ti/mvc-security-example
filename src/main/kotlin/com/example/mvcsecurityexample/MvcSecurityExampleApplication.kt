864fca3e4bc3914e5199a9515cdb1853bc55af07
엄태권 <akfls221@gmail.com>
2024-07-09 01:56:35 +0900

    init project

---
+++ src/main/kotlin/com/example/mvcsecurityexample/MvcSecurityExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mvcsecurityexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MvcSecurityExampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<MvcSecurityExampleApplication>(*args)
+}
