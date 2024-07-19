f2b1c70e3507cd8a68c34f9210a5430d558818ac
엄태권 <akfls221@gmail.com>
2024-07-19 22:59:25 +0900

    feat : add LoginFilter Chain config

--- src/main/kotlin/com/example/mvcsecurityexample/security/SecurityConfig.kt
+++ src/main/kotlin/com/example/mvcsecurityexample/security/SecurityConfig.kt
@@ -1,29 +1,40 @@
 package com.example.mvcsecurityexample.security
 
+import com.example.mvcsecurityexample.security.filter.ExampleUserNamePasswordFilter
+import com.example.mvcsecurityexample.security.filter.ExampleUserNamePasswordProvider
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+) {
 
     @Bean
     fun filerChain(http: HttpSecurity): SecurityFilterChain {
         http {
+            securityMatcher("/login")
             csrf { disable() }
             httpBasic { disable() }
             formLogin { disable() }
-            sessionManagement { sessionCreationPolicy = SessionCreationPolicy.NEVER }
-            authorizeRequests {
-                authorize(anyRequest, authenticated)
-            }
+            sessionManagement { sessionCreationPolicy = SessionCreationPolicy.STATELESS }
+            addFilterAt<UsernamePasswordAuthenticationFilter>(ExampleUserNamePasswordFilter(ProviderManager(exampleUserNamePasswordProvider())))
         }
         return http.build()
+    }
+
+    @Bean
+    fun exampleUserNamePasswordProvider(): AuthenticationProvider {
+        return ExampleUserNamePasswordProvider()
     }
 }
