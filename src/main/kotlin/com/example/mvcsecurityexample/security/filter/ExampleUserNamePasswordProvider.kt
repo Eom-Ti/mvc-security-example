fd6b836331f5734d9c2f89689f70e2abbd570a98
엄태권 <akfls221@gmail.com>
2024-07-26 21:06:56 +0900

    feat : add ExampleUserNamePasswordPRovider

---
+++ src/main/kotlin/com/example/mvcsecurityexample/security/filter/ExampleUserNamePasswordProvider.kt
@@ -0,0 +1,30 @@
+package com.example.mvcsecurityexample.security.filter
+
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+
+class ExampleUserNamePasswordProvider(
+
+    private val tokenProvider: TokenProvider,
+    private val userAuthenticationService: UserAuthenticationService
+
+): AuthenticationProvider {
+
+    override fun authenticate(authentication: Authentication?): Authentication {
+        val token = authentication as ExampleAuthenticationToken
+        val accessToken = token.token.accessToken
+
+        verifyToken(accessToken)
+
+    }
+
+    override fun supports(authentication: Class<*>?): Boolean {
+        return authentication?.isAssignableFrom(ExampleAuthenticationToken::class.java) ?: false
+    }
+
+
+    private fun verifyToken(accessToken: String) {
+
+    }
+}
