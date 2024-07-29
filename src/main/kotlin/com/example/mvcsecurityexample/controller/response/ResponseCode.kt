9331989d4ceb73e5d5bd3288fc2592bcb50c2992
엄태권 <akfls221@gmail.com>
2024-07-29 23:18:16 +0900

    feat : add 200 Success Code

--- src/main/kotlin/com/example/mvcsecurityexample/controller/response/ResponseCode.kt
+++ src/main/kotlin/com/example/mvcsecurityexample/controller/response/ResponseCode.kt
@@ -1,4 +1,8 @@
 package com.example.mvcsecurityexample.controller.response
 
-enum class ResponseCode {
+enum class ResponseCode(
+    val code: Int,
+    val msg: String
+) {
+    SUCCESS(200, "SUCCESS")
 }
