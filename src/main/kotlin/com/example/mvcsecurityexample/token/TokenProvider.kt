42225c69bd9711cd2d05a04d5d91fd5eeccc3916
엄태권 <akfls221@gmail.com>
2024-07-31 22:09:27 +0900

    feat : add interface method

--- src/main/kotlin/com/example/mvcsecurityexample/token/TokenProvider.kt
+++ src/main/kotlin/com/example/mvcsecurityexample/token/TokenProvider.kt
@@ -2,5 +2,9 @@ package com.example.mvcsecurityexample.token
 
 interface TokenProvider {
 
+    fun createToken(tokenPayload: TokenPayload): ExampleToken
 
+    fun accessTokenVerify(accessToken: String)
+
+    fun getPayload(accessToken: String): TokenPayload
 }
