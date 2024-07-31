608a1b9444ce8791dcbc2755221286f74756c882
엄태권 <akfls221@gmail.com>
2024-07-31 22:26:07 +0900

    feat : fix tokenProperties

--- src/main/kotlin/com/example/mvcsecurityexample/token/TokenProperties.kt
+++ src/main/kotlin/com/example/mvcsecurityexample/token/TokenProperties.kt
@@ -1,10 +1,32 @@
 package com.example.mvcsecurityexample.token
 
+import io.jsonwebtoken.security.Keys
 import org.springframework.boot.context.properties.ConfigurationProperties
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
+import java.util.*
+import javax.crypto.SecretKey
 
 @ConfigurationProperties("jwt")
 class TokenProperties(
-    val secretKey: String,
-    val accessTokenExpiredMinute: Long
+    private val secretKey: String,
+    private val accessTokenExpiredMinute: Long
 ) {
+
+    fun getSecretKey(): SecretKey {
+        return Keys.hmacShaKeyFor(secretKey.toByteArray())
+    }
+
+    fun getAccessTokenExpiredDate(): Date {
+        return getTokenExpiredDate(accessTokenExpiredMinute)
+
+    }
+
+    private fun getTokenExpiredDate(accessTokenExpiredMinute: Long): Date {
+        val instant = ZonedDateTime.now(ZoneOffset.UTC)
+            .plusMinutes(accessTokenExpiredMinute)
+            .toInstant()
+
+        return Date.from(instant)
+    }
 }
