84fc20ba1202f9e4fe4c4680d5dff937f78840a1
엄태권 <akfls221@gmail.com>
2024-07-31 22:03:07 +0900

    feat : add Token properteis config

--- src/main/kotlin/com/example/mvcsecurityexample/token/TokenProperties.kt
+++ src/main/kotlin/com/example/mvcsecurityexample/token/TokenProperties.kt
@@ -1,5 +1,8 @@
 package com.example.mvcsecurityexample.token
 
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties("jwt")
 class TokenProperties(
     val secretKey: String,
     val accessTokenExpiredMinute: Long
