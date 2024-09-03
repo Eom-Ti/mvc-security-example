c2a7af252a058afbad0b1d12a9cccc487cc80fc0
엄태권 <akfls221@gmail.com>
2024-09-03 23:27:34 +0900

    feat : add mock user service method

--- src/main/kotlin/com/example/mvcsecurityexample/service/UserAuthenticationService.kt
+++ src/main/kotlin/com/example/mvcsecurityexample/service/UserAuthenticationService.kt
@@ -1,7 +1,14 @@
 package com.example.mvcsecurityexample.service
 
+import com.example.mvcsecurityexample.domain.User
 import org.springframework.stereotype.Service
 
 @Service
-class UserAuthenticationService {
+class UserAuthenticationService(
+
+) {
+
+    fun findByUserId(userId: String): User {
+        return User("t", 1)
+    }
 }
