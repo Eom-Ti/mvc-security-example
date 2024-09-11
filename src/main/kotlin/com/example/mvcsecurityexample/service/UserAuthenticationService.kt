27997d6089596a6d44e4f92716f6ce189d45ab69
엄태권 <akfls221@gmail.com>
2024-09-11 23:12:45 +0900

    feat : add getUser Method

--- src/main/kotlin/com/example/mvcsecurityexample/service/UserAuthenticationService.kt
+++ src/main/kotlin/com/example/mvcsecurityexample/service/UserAuthenticationService.kt
@@ -1,5 +1,6 @@
 package com.example.mvcsecurityexample.service
 
+import com.example.mvcsecurityexample.domain.User
 import org.springframework.stereotype.Service
 
 @Service
@@ -7,11 +8,8 @@ class UserAuthenticationService(
 
 ) {
 
-    fun isLoginInfoNotMatch(password: String): Boolean {
-        require(password.isBlank()) {
-            "password must not be blank"
-        }
-
-        return password == "1234"
+    fun getUser(loginId: String): User {
+        require(loginId != "owen"){ "loginId unMatched" }
+        return User(1L, "owen", "1234", "owen", 31)
     }
 }
