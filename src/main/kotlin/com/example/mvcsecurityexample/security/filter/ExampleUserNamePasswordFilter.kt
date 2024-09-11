1cc113faf7e4ff8d043e3eb2bd1b58165d5094e4
엄태권 <akfls221@gmail.com>
2024-09-11 22:33:59 +0900

    fix : remove usUsed import

--- src/main/kotlin/com/example/mvcsecurityexample/security/filter/ExampleUserNamePasswordFilter.kt
+++ src/main/kotlin/com/example/mvcsecurityexample/security/filter/ExampleUserNamePasswordFilter.kt
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
-import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.AuthenticationServiceException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
