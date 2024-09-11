87db2e7bfbbc7472627ae4c944c9f9782e81bc6e
엄태권 <akfls221@gmail.com>
2024-09-11 23:13:00 +0900

    feat : add checkPasswordMatch method

--- src/main/kotlin/com/example/mvcsecurityexample/domain/User.kt
+++ src/main/kotlin/com/example/mvcsecurityexample/domain/User.kt
@@ -2,11 +2,22 @@ package com.example.mvcsecurityexample.domain
 
 class User(
 
-    val userId: String,
+    val userId: Long,
+
+    val loginId: String,
+
+    val password: String,
 
     val userName: String,
 
     val userAge: Int
 ) {
 
+    fun checkPasswordMatch(password: String): Boolean {
+        require(password.isBlank()) {
+            "password must not be blank"
+        }
+
+        return password == "1234"
+    }
 }
